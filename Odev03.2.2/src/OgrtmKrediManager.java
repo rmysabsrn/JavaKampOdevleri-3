f99c146f658c7d63fe891f19f5fd1303ffd873bf
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-10-10 23:12:45 +0300

    abstract class

---
+++ Odev03.2.2/src/OgrtmKrediManager.java
@@ -0,0 +1,2 @@
+public class OgrtmKrediManager extends BaseKrediManager{
+}
---
+++ Odev03.2.3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        FemaleGameCalculator femaleGameCalculator =new FemaleGameCalculator();
+        femaleGameCalculator.hesapla();
+
+        GameCalculator gameCalculator = new FemaleGameCalculator();
+
+
+    }
+
+}
