90de3187cdbfa375acc655d1ce32d2669e76ef57
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-10-11 13:01:33 +0300

    Demo-çoklu implementasyon

---
+++ Odev03.2.7/src/Worker.java
@@ -0,0 +1,17 @@
+//bizim çalışanıımız
+public class Worker implements IPayable ,IWorkable, IEatable {
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+}
